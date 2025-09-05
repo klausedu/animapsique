2e9ad9a667aa4afa08ef92277dd11383cd2a7bb9
klaused <klaused@gmail.com>
2025-09-05 15:53:25 -0300

    Update sala_atendimento.php

--- area_logada/paciente/sala_atendimento.php
+++ area_logada/paciente/sala_atendimento.php
@@ -37,7 +37,7 @@ require_once 'templates/header.php';
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M4 8V4m0 0h4M4 4l5 5m11-1V4m0 0h-4m4 0l-5 5M4 16v4m0 0h4m-4 0l5-5m11 5v-4m0 4h-4m4 0l-5-5" />
                 </svg>
-                Ecrã Cheio
+                Tela Cheia
             </button>
             </div>
         <p class="text-gray-600 mb-6">Sua sessão está prestes a começar. Permita o acesso à sua câmera e microfone quando solicitado pelo navegador.</p>
