c0274a6ed84129c3f13d58541c1deecafcdddd95
klaused <klaused@gmail.com>
2025-10-01 11:12:29 -0300

    Update publicacoes_academicas.php

--- publicacoes_academicas.php
+++ publicacoes_academicas.php
@@ -30,13 +30,22 @@ require_once 'templates/header_publico.php';
                 <?php foreach ($publicacoes as $pub): ?>
                     <div x-data="{ open: false }" class="bg-gray-50 rounded-lg shadow-sm">
                         <button @click="open = !open" class="w-full flex justify-between items-center p-4 text-left text-lg font-semibold text-gray-700">
-                            <span class="flex items-center">
+                            <span class="flex items-center pr-4">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-3 text-teal-600 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.246 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
                                 </svg>
                                 <?php echo htmlspecialchars($pub['titulo']); ?>
                             </span>
-                            <svg :class="{'transform rotate-180': open}" class="w-7 h-7 text-gray-500 transition-transform duration-300" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
+                            
+                            <div class="flex-shrink-0">
+                                <svg x-show="!open" xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-[var(--cor-primaria)]" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                                <svg x-show="open" xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-[var(--cor-primaria)]" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" style="display: none;">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                            </div>
+
                         </button>
                         <div x-show="open" x-transition class="p-4 pt-0 text-gray-600 prose max-w-none">
                             <?php echo $pub['texto']; ?>
